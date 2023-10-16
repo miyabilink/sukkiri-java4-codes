b3a60ac93ca312bf7d6fc42c0296995ffb02d51c
Takuma SUGAWARA <sugawara@miyabilink.jp>
2023-10-16 14:42:21 +0900

    不要な改行が含まれているコードを修正

--- chap00/code00-03/Main.java
+++ chap00/code00-03/Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("湊くんかっこいい！最高！);
+    System.out.println("湊くんかっこいい！最高！);
   }
 }
--- chap00/code00-04/Main.java
+++ chap00/code00-04/Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("菅原拓真です");
     System.out.println("31歳です");
-    System.out.println("お酒が好きです");
+    System.out.println("お酒が好きです");
   }
 }
