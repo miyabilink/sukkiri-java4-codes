2307e40d04fcf0ab7fd2672c1a609d747837e761
Takuma SUGAWARA <sugawara@miyabilink.jp>
2025-08-17 10:10:00 +0900

    13章の練習問題にコメントを追加

--- chapD/quizans13-03/Wizard.java
+++ chapD/quizans13-03/Wizard.java
@@ -5,7 +5,7 @@ public class Wizard {
   private Wand wand;
   public void heal(Hero h) {
     int basePoint = 10;                                            // 基本回復ポイント
-    int recovPoint = (int)(basePoint * this.getWand().getPower()); // 杖による増幅
+    int recovPoint = (int)(basePoint * this.getWand().getPower()); // 杖の魔力による増幅
     h.setHp(h.getHp() + recovPoint);                               // 勇者のHPを回復する
     System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
   }
